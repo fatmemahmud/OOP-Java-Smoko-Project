9be28ce91fb5118dec8448dabc856a5fb3799371
fatmemahmud <59645255+fatmemahmud@users.noreply.github.com>
2020-02-16 19:49:22 +0300

    Add files via upload

--- SmokoProject/src/es/games/smoko/Cell.java
+++ SmokoProject/src/es/games/smoko/Cell.java
@@ -2,7 +2,7 @@ package es.games.smoko;
 
 public class Cell {
 
-    final static int CELL_TYPE_EMPTY = 0, CELL_TYPE_FOOD = 10, CELL_TYPE_SNAKE_NODE = 20;
+    final static int CELL_TYPE_EMPTY = 0, CELL_TYPE_FOOD = 10, CELL_TYPE_SMOKO_NODE = 20;
     final int row, col;
     int type;
 
