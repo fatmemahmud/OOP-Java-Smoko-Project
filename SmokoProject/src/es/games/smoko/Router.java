716c027991cb316afa5290791790fa2ae7ec0574
fatmemahmud <59645255+fatmemahmud@users.noreply.github.com>
2020-02-29 22:35:38 +0200

    Update Router.java

--- SmokoProject/src/es/games/smoko/Router.java
+++ SmokoProject/src/es/games/smoko/Router.java
@@ -1,57 +1,11 @@
 package es.games.smoko;
 
-public class Router {
+public interface Router {
 
-	   public static final int DIRECTION_NONE = 0, DIRECTION_RIGHT = 1, DIRECTION_LEFT = -1, DIRECTION_UP = 2, DIRECTION_DOWN = -2;
-	    private Smoko smoko;
-	    private Board board;
-	    private int direction;
-	    private boolean gameOver;
-
-	    public Router(Smoko smoko, Board board) {
-	        this.smoko = smoko;
-	        this.board = board;
-	    }
-
-	    public void setDirection(int direction) {
-	        this.direction = direction;
-	    }
-
-	    public void update() {
-	        if (!gameOver) {
-	            if (direction != DIRECTION_NONE) {
-	                Cell nextCell = getNextCell(smoko.head);
-
-	                if (smoko.checkCrash(nextCell)) {
-	                    setDirection(DIRECTION_NONE);
-	                    gameOver = true;
-	                } else {
-	                	smoko.move(nextCell);
-	                    if (nextCell.type == Cell.CELL_TYPE_FOOD) {
-	                    	smoko.grow();
-	                        board.generateFood();
-	                    }
-	                }
-	            }
-	        }
-	    }
-
-	    private Cell getNextCell(Cell currentPosition) {
-	        int row = currentPosition.row;
-	        int col = currentPosition.col;
-
-	        if (direction == DIRECTION_RIGHT) {
-	            col++;
-	        } else if (direction == DIRECTION_LEFT) {
-	            col--;
-	        } else if (direction == DIRECTION_UP) {
-	            row--;
-	        } else if (direction == DIRECTION_DOWN) {
-	            row++;
-	        }
-
-	        Cell nextCell = board.cells[row][col];
-
-	        return nextCell;
-	    }
+	public static final int LEFT = -1;
+	public static final int RIGHT = 1;
+	public static final int UP = 2; 
+	public static final int DOWN = -2;
+	
+	   
 	}
